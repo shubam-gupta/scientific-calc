9f7612a4d75be46a75e1c518cbd4c36a186cc922
shubi <shubam.gupta@iiitb.org>
2020-11-24 16:50:44 +0530

    Initial Commit

---
+++ src/main/java/com/testingproject/scientificcalc/ScientificCalcApplication.java
@@ -0,0 +1,13 @@
+package com.testingproject.scientificcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScientificCalcApplication.class, args);
+	}
+
+}
